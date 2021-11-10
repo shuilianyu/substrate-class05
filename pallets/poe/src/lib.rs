c914f48a7f92bd95e7d5ebcd905de55bf0a8d606
shuilianyu <13783548908@163.com>
2021-11-10 09:50:26 -0800

    new commit

--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -23,12 +23,12 @@ pub mod pallet {
     #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
 
-    #[pallet::pallet]
+    #[pallet::storage]
     #[pallet::getter(fn proofs)]
     pub type Proofs<T: Config> = StorageMap<
-        _;
-        Blake2_128Concat;
-        Vec<u8>;
+        _,
+        Blake2_128Concat,
+        Vec<u8>,
         (T::AccountId, T::BlockNumber)
     >;
 
@@ -103,8 +103,6 @@ pub mod pallet {
             ensure!(owner == sender, Error::<T>::NotClaimOwner);
 
             Proofs::<T>::remove(&claim);
-
-            let to = ensure_signed(origin)?;
 
             Proofs::<T>::insert(
                 &claim,
